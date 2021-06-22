aa049a6b9b4c46222ee06da8239f5047e0529c08
shambhawipandey29 <55096583+shambhawipandey29@users.noreply.github.com>
2021-06-23 00:01:42 +0100

    Update committee_active_learner.py

--- committee_active_learner.py
+++ committee_active_learner.py
@@ -28,7 +28,7 @@ from statsmodels.tsa.arima_model import ARIMA
 #df=pd.read_csv("./dataset/train.csv")
 df=pd.read_csv("./dataset/LMTO_B_new.csv")
 #df.drop(["name", "OH"], axis=1, inplace=True)
-df.drop(["name", "OH", "U_param", "spin","Tolerance_factor", "Octahedral_factor", "ionic_ration_AO", "ionic_ration_BO", "average_ionic_radius1"], axis=1, inplace=True)
+df.drop(["name", "OH", "Tolerance_factor", "Octahedral_factor", "ionic_ration_AO", "ionic_ration_BO", "average_ionic_radius1"], axis=1, inplace=True)
 
 target_column = "O"
 x_input = df.drop(target_column, axis = 1)
